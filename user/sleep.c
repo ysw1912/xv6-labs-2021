caae151f47819b95fd434584473d240f565a8073
Stewie Yang <ysw1912>
2022-04-12 01:30:03 +0800

    impl sleep

--- user/sleep.c
+++ user/sleep.c
@@ -1,4 +1,13 @@
-//
-// Created by 79337 on 2022/4/11.
-//
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
 
+int main(int argc, char* argv[]) {
+  if (argc != 2) {
+    fprintf(2, "Usage: sleep seconds\n");
+    exit(1);
+  }
+  int sec = atoi(argv[1]);
+  sleep(sec);
+  exit(0);
+}
