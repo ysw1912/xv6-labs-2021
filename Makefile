e6e25c8b7db46164b1a87a9714a21dc77ec14308
Stewie Yang <ysw1912>
2022-05-17 21:22:29 +0800

    impl trace

--- Makefile
+++ Makefile
@@ -193,6 +193,7 @@ UPROGS=\
 	$U/_sh\
 	$U/_sleep\
 	$U/_stressfs\
+	$U/_trace\
 	$U/_usertests\
 	$U/_grind\
 	$U/_wc\
--- kernel/proc.c
+++ kernel/proc.c
@@ -301,6 +301,9 @@ fork(void)
       np->ofile[i] = filedup(p->ofile[i]);
   np->cwd = idup(p->cwd);
 
+  // Copy trace mask.
+  np->trace_mask = p->trace_mask;
+
   safestrcpy(np->name, p->name, sizeof(p->name));
 
   pid = np->pid;
